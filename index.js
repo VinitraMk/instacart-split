bb8f78572deca50abf8f1fd83d7b5cf1ea6f3240
Vinitra Muralikrishnan <vinitramk@gmail.com>
2024-03-18 04:40:31 -0400

    cleaning up index.js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-/* exported gapiLoaded */
-/* exported gisLoaded */
-/* exported handleAuthClick */
-/* exported handleSignoutClick */
 
-// TODO(developer): Set to client ID and API key from the Developer Console
-const CLIENT_ID = '812777760600-it369gqkpl2i3p6uue6lka22lklqhrv8.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyDez1eaAz4HGk4UiwPsNXfiHTYt8ShowoA';
-
-// Discovery doc URL for APIs used by the quickstart
-const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest';
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
 const MATE1 = 'urja';
 const MATE2 = 'vinitra';
 const MATE3 = 'srimathi';
 
-let tokenClient;
-let gapiInited = false;
-let gisInited = false;
-let instacartId = window.localStorage.getItem('instacart_id') ?? '';
 const messages = [];
 const message_contents = [];
 let selectedOrderContent = null;
@@ -32,8 +14,6 @@ let individualShares = {
 }
 let selectedOrderTotal = [];
 let unEvenShares = [];
-//document.getElementById('authorize_button').style.visibility = 'hidden';
-//document.getElementById('signout_button').style.visibility = 'hidden';
 
 function readFile(event) {
     const f = event.target.files[0];
@@ -41,7 +21,6 @@ function readFile(event) {
         const r = new FileReader();
         r.onload = function (e) {
             const contents = e.target.result;
-            //document.getElementById("ReadResult").innerHTML = contents;
             readOrder(contents);
         }
         r.readAsText(f);
@@ -52,20 +31,14 @@ function readFile(event) {
 
 function readOrder(htmlContent) {
     const parser = new DOMParser();
-    //const select = document.getElementById('order_select');
-    //const selectedMessage = message_contents.filter(mc => mc.id === select.value)[0];
     const html = parser.parseFromString(htmlContent, 'text/html');
     const itemMain = html.querySelector('main');
     let itemDivs = Array.from(itemMain.querySelectorAll('table[class="items delivered"] td[class="order-item"]'));
     const itemTotals = Array.from(itemMain.querySelector('div.totals-container').querySelectorAll('table > tbody > tr'));
-    //const totalIndex = innerTexts.findIndex(x => x.includes('Order Totals'));
     selectedOrderContent = itemDivs.map(d => d.childNodes[1]);
     itemDivs = Array.from(itemMain.querySelectorAll('table[class="items adjustments"] td[class="order-item"]')).map(d => d.childNodes[1]);
     selectedOrderContent = selectedOrderContent.concat(itemDivs);
-    console.log('order items', selectedOrderContent)
 
-    //const orderBrkouts = itemDivs[totalIndex].querySelector('table > tbody').querySelectorAll('tr td:nth-child(1)');
-    //const orderBrkoutsPrices = itemDivs[totalIndex].querySelector('table > tbody').querySelectorAll('tr td:nth-child(2)');
     selectedOrderTotal = [];
     for(let i = 0; i < itemTotals.length; i++) {
         selectedOrderTotal.push({
@@ -144,7 +117,6 @@ function populateOrderTable() {
         })
     })
     document.getElementById('share_table').appendChild(tableBody);
-    console.log(selectedOrderShares);
 }
 
 function populateOrderTotal() {
@@ -179,8 +151,6 @@ function setCheckboxValue(event) {
 }
 
 function calculateShares() {
-    //console.log(selectedOrderShares);
-    //console.log(selectedOrderTotal);
     individualShares[MATE1] = 0.0;
     individualShares[MATE2] = 0.0;
     individualShares[MATE3] = 0.0;
